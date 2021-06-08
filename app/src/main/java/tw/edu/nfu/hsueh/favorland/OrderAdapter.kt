fc4127d4806d1ecd5f8c211000e8f14e25089a2e
yoyo <ya90312@gmail.com>
2021-06-08 22:31:15 +0800

    新增orderRecycler使用的adater以及dataclass並完成與畫面繫節的code

---
+++ app/src/main/java/tw/edu/nfu/hsueh/favorland/OrderAdapter.kt
@@ -0,0 +1,34 @@
+package tw.edu.nfu.hsueh.favorland
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class OrderAdapter(private val data:ArrayList<OrderModel>):RecyclerView.Adapter<OrderAdapter.ViewHolder>() {
+    class ViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
+        val tv_orderName = itemView.findViewById<TextView>(R.id.tv_orderName)
+        val imgView_minus = itemView.findViewById<ImageView>(R.id.imgView_minus)
+        val imgView_add = itemView.findViewById<ImageView>(R.id.imgView_add)
+        val tv_count = itemView.findViewById<TextView>(R.id.tv_count)
+    }
+    override fun onCreateViewHolder(ViewGroup: ViewGroup, viewType: Int): OrderAdapter.ViewHolder {
+        val v = LayoutInflater.from(ViewGroup.context).inflate(R.layout.order_view,ViewGroup,false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: OrderAdapter.ViewHolder, position: Int) {
+        holder.tv_orderName.text = data[position].dish
+        holder.tv_count.text = data[position].count.toString()
+        holder.imgView_minus.setOnClickListener {
+            holder.tv_count.text = (data[position].count -1).toString()
+        }
+        holder.imgView_add.setOnClickListener {
+            holder.tv_count.text = (data[position].count +1).toString()
+        }
+    }
+
+    override fun getItemCount(): Int = data.size
+}
---
+++ app/src/main/java/tw/edu/nfu/hsueh/favorland/OrderModel.kt
@@ -0,0 +1,7 @@
+package tw.edu.nfu.hsueh.favorland
+
+data class OrderModel(
+    val dish:String,
+    val price:Int,
+    val count:Int,
+            )
